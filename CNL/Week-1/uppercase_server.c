b108a790d35f117f096cc9a1253620725d07947c
RYXVL <95095676+RYXVL@users.noreply.github.com>
2022-07-31 14:39:07 +0530

    final

--- CNL/Week-1/uppercase_server.c
+++ CNL/Week-1/uppercase_server.c
@@ -8,6 +8,8 @@
 #include <arpa/inet.h>
 #include <ctype.h>
 
+#define PORTNO 6001
+
     int sockfd, newsockfd, portno, clilen, n = 1;
     struct sockaddr_in seraddr, cliaddr;
     int i, value;
@@ -17,7 +19,8 @@ void createServerSocket() {
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
     seraddr.sin_family = AF_INET;
     seraddr.sin_addr.s_addr = inet_addr("127.0.0.1");
-    seraddr.sin_port = htons(5400);
+    seraddr.sin_port = htons(PORTNO);
+    printf("IP: 127.0.0.1 ; Ephimeral Port Number: %d\n", PORTNO);
     bind(sockfd, (struct sockaddr*)&seraddr, sizeof(seraddr));
     listen(sockfd, 5);
 }
@@ -29,7 +32,15 @@ void performServerTask() {
         newsockfd = accept(sockfd, (struct sockaddr*)&cliaddr, &clilen);
         n = read(newsockfd, buff, sizeof(buff));
         printf("Message from client: %s\n", buff);
-        buff = toupper(buff);
+        int i=0;
+        while(buff[i]) {
+            if(buff[i]>96 && buff[i]<122) buff[i]-=32;
+            else {
+                i++;
+                continue;
+            }
+            i++;
+        }
         n = write(newsockfd, buff, sizeof(buff));
     }
 }
