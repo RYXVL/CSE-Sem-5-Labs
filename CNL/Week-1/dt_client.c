c186702cce9cc282d598e77f1257b84642a36752
RYXVL <95095676+RYXVL@users.noreply.github.com>
2022-07-31 14:41:42 +0530

    final

--- CNL/Week-1/dt_client.c
+++ CNL/Week-1/dt_client.c
@@ -6,19 +6,19 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <arpa/inet.h>
-
 #include <time.h>
 
+#define PORTNO 4507
+
 int len, result, sockfd, n = 1;
-    struct sockaddr_in address;
-    char ch[256], buff[2000];
-    time_t t;
+struct sockaddr_in address;
+char ch[256], buff[2000];
 
 void createClientSocket() {
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = inet_addr("127.0.0.1");
-    address.sin_port = htons(5410);
+    address.sin_port = htons(PORTNO);
     len = sizeof(address);
     result = connect(sockfd, (struct sockaddr*)&address, len);
     if(result == -1) {
@@ -32,12 +32,9 @@ void performClientTask() {
     gets(ch);
     ch[strlen(ch)] = '\0';
     write(sockfd, ch, strlen(ch));
-    printf("Time: ");
     while(n) {
-        n = read(sockfd, &t, sizeof(t));
-        // printf("%ld", sizeof(buff));
-        // puts(buff);
-        printf("\n Current date and time is : %s", ctime(&t));
+        n = read(sockfd, ch, sizeof(ch));
+        printf("\n Current date and time is : %s", ch);
     }
 }
 
