ab8c65a9852c8c22145785e8276784818caa353a
Ryan <sojan.ryan@gmail.com>
2022-08-12 23:28:21 +0530

    more changes

--- CDL/Week-3/la.c
+++ CDL/Week-3/la.c
@@ -11,12 +11,11 @@ struct token {
 FILE *fptr;
 char filename[50], buff[50], c;
 int ind = 0;
-struct token t;
+struct token *t;
 int row = 1, col = 1;
+int j;
 
-struct token getToken(FILE* fptr) {
-	struct token newToken;
-	char keys[32][10] = {"auto", "break", "case", "char", 
+char keys[32][10] = {"auto", "break", "case", "char", 
 					   "const", "continue", "default", "do", 
 					   "double", "else", "enum", "extern", 
 					   "float", "for", "goto", "if", 
@@ -24,47 +23,106 @@ struct token getToken(FILE* fptr) {
 					   "short", "signed", "sizeof", "static", 
 					   "struct", "switch", "typedef", "union", 
 					   "unsigned", "while", "volatile", "void"};
-	c = fgetc(fptr);
-	if(c==EOF) exit(0);
-	// for row and col - check after correct output return
-	// if(c=='\n') {
-	// 	col++;
-	// 	row = 1;
-	// }
-	// else row++;
 
-	// skips all preprocessor directives
-	if(c=='#')
-		while(c!='\n')
-			c = fgetc(fptr);
-	// skips all comments
-	if(c=='/') {
-		c = fgetc(fptr);
-		if(c=='/')
-			while(c!='\n')
-				c = fgetc(fptr);
-		else if(c=='*')
-			while(1) {
-				while(c!='*')
-					c = fgetc(fptr);
-				c = fgetc(fptr);
-				if(c=='/') break;
-				else continue;
+char symbol[8][1] = {'[', ']', '(', ')', '{', '}', ',', ';'};
+
+int findSymbol(char ch) {
+	for(int i=0; i<8; i++)
+		if(symbol[i][0]==ch) return 1;
+	return 0;
+}
+
+// char opr[][] = {'+', '-', '*', '/', '%', ++, --, };
+
+struct token* getToken(FILE* fptr) {
+	struct token* newToken = (struct token*)malloc(sizeof(*newToken));
+	ind = 0;
+	c = fgetc(fptr);
+	loop:
+		if(c==EOF) exit(0);
+		if(c==' ' || c=='\t') {
+			while(c==' ' || c=='\t') {
+				c=fgetc(fptr);
+				row++;
 			}
-	}
-	while(isalpha(c)==1) {
-		buff[ind++] = c;
-		c = fgetc(fptr);
-	}
-	buff[ind]='\0';
-	for(int j = 0; j<32; j++) {
-		if(strcmp(buff, keys[j])==0) {
-			strcpy(newToken.tokenName,"Keyword");
-			newToken.row = newToken.col = 123;
-			// fputc('\n', fptr2);
-			break;
+			goto loop;
 		}
-	}
+		if(c == '\n') {
+			while(c=='\n') {
+				col++;
+				c=fgetc(fptr);
+			}
+			goto loop;
+		}
+		if(c=='#') {
+			while(c!='\n') {
+				c = fgetc(fptr);
+				row++;
+			}
+			col++;
+			c = fgetc(fptr);
+			row++;
+			goto loop;
+		}
+		if(c=='/') {
+			c = fgetc(fptr);
+			row++;
+			if(c=='/') {
+				while(c!='\n') {
+					c = fgetc(fptr);
+					row++;
+				}
+				c = fgetc(fptr);
+			}
+			else if(c=='*') {
+				while(1) {
+					while(c!='*')
+						c = fgetc(fptr);
+					c = fgetc(fptr);
+					if(c=='/') break;
+					else continue;
+				}
+				c = fgetc(fptr);
+			}
+			goto loop;
+		}
+		if(isalpha(c)!=0) {
+			while(isalpha(c)!=0) {
+				buff[ind++] = c;
+				c = fgetc(fptr);
+			}
+			fseek(fptr, -1, SEEK_CUR);
+			buff[ind]='\0';
+			for(j = 0; j<32; j++) {
+				if(strcmp(buff, keys[j])==0) {
+					strcpy(newToken->tokenName,buff);
+					memset(buff, 0, 50);
+					newToken->row = newToken->col = 123;
+					break;
+				}
+			}
+			if(j==32) {
+			    strcpy(newToken->tokenName,buff);
+				memset(buff, 0, 50);
+				newToken->row = newToken->col = 123;
+			}
+		}
+		else if(isdigit(c)!=0) {
+			while(isdigit(c)!=0) {
+				buff[ind++] = c;
+				c = fgetc(fptr);
+			}
+			fseek(fptr, -1, SEEK_CUR);
+			buff[ind]='\0';
+			strcpy(newToken->tokenName,buff);
+			memset(buff, 0, 50);
+			newToken->row = newToken->col = 123;
+		}
+		else if(findSymbol(c)==1) {
+			newToken->tokenName[0] = c;
+			newToken->tokenName[1] = '\0';
+			newToken->row = newToken->col = 123;
+		}
 	return newToken;
 }
 
@@ -76,12 +134,10 @@ int main() {
 		printf("The file does not exist.\n");
 		exit(0);
 	}
-	t = getToken(fptr);
-	while(t.tokenName!=EOF) {
-		printf("<%s, %d, %d>", t.tokenName, t.row, t.col);
+	while(1) {
 		t = getToken(fptr);
+		printf("<%s, %d, %d>\n", t->tokenName, t->row, t->col);
 	}
-	// getToken(fptr);
 	fclose(fptr);
 	return 0;
 }
