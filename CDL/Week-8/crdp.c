28e424610afaa8a818fb8d1af76afec3cb6bfb05
RYXVL <95095676+RYXVL@users.noreply.github.com>
2022-10-04 11:23:29 +0530

    final

--- CDL/Week-8/crdp.c
+++ CDL/Week-8/crdp.c
@@ -55,12 +55,12 @@ int findfollowStatement(char *str) {
 }
 
 char* firstAssignStat = "id";
-char followAssignStat[2][10] = {"id", "}"};
-int findfollowAssignStat(char *str) {
-	for(int i=0; i<2; i++)
-		if(strcmp(followAssignStat[i], str)==0) return i;
-	return -1;
-}
+char* followAssignStat = ";";
+// int findfollowAssignStat(char *str) {
+// 	for(int i=0; i<2; i++)
+// 		if(strcmp(followAssignStat[i], str)==0) return i;
+// 	return -1;
+// }
 
 char firstExpn[2][10] = {"id", "NUM"};
 int findfirstExpn(char *str) {
@@ -68,12 +68,12 @@ int findfirstExpn(char *str) {
 		if(strcmp(firstExpn[i], str)==0) return i;
 	return -1;
 }
-char followExpn[2][10] = {"id", "}"};
-int findfollowExpn(char *str) {
-	for(int i=0; i<2; i++)
-		if(strcmp(followExpn[i], str)==0) return i;
-	return -1;
-}
+char* followExpn = ";";
+// int findfollowExpn(char *str) {
+// 	for(int i=0; i<2; i++)
+// 		if(strcmp(followExpn[i], str)==0) return i;
+// 	return -1;
+// }
 
 char firstSimpleExpn[2][10] = {"id", "NUM"};
 int findfirstSimpleExpn(char *str) {
@@ -81,9 +81,9 @@ int findfirstSimpleExpn(char *str) {
 		if(strcmp(firstSimpleExpn[i], str)==0) return i;
 	return -1;
 }
-char followSimpleExpn[8][10] = {"==", "!=", "<=", ">=", "<", ">", "id", "}"};
+char followSimpleExpn[7][10] = {"==", "!=", "<=", ">=", "<", ">", ";"};
 int findfollowSimpleExpn(char *str) {
-	for(int i=0; i<8; i++)
+	for(int i=0; i<7; i++)
 		if(strcmp(followSimpleExpn[i], str)==0) return i;
 	return -1;
 }
@@ -94,9 +94,9 @@ int findfirstTerm(char *str) {
 		if(strcmp(firstTerm[i], str)==0) return i;
 	return -1;
 }
-char followTerm[10][10] = {"+", "-", "==", "!=", "<=", ">=", "<", ">", "id", "}"};
+char followTerm[9][10] = {"+", "-", "==", "!=", "<=", ">=", "<", ">", ";"};
 int findfollowTerm(char *str) {
-	for(int i=0; i<10; i++)
+	for(int i=0; i<9; i++)
 		if(strcmp(followTerm[i], str)==0) return i;
 	return -1;
 }
@@ -107,9 +107,9 @@ int findfirstFactor(char *str) {
 		if(strcmp(firstFactor[i], str)==0) return i;
 	return -1;
 }
-char followFactor[13][10] = {"*", "/", "%", "+", "-", "==", "!=", "<=", ">=", "<", ">", "id", "}"};
+char followFactor[12][10] = {"*", "/", "%", "+", "-", "==", "!=", "<=", ">=", "<", ">", ";"};
 int findfollowFactor(char *str) {
-	for(int i=0; i<13; i++)
+	for(int i=0; i<12; i++)
 		if(strcmp(followFactor[i], str)==0) return i;
 	return -1;
 }
@@ -133,9 +133,9 @@ int findfirstTprime(char *str) {
 		if(strcmp(firstTprime[i], str)==0) return i;
 	return -1;
 }
-char followTprime[10][10] = {"+", "-", "==", "!=", "<=", ">=", "<", ">", "id", "}"};
+char followTprime[9][10] = {"+", "-", "==", "!=", "<=", ">=", "<", ">", ";"};
 int findfollowTprime(char *str) {
-	for(int i=0; i<10; i++)
+	for(int i=0; i<9; i++)
 		if(strcmp(followTprime[i], str)==0) return i;
 	return -1;
 }
@@ -146,9 +146,9 @@ int findfirstSeprime(char *str) {
 		if(strcmp(firstSeprime[i], str)==0) return i;
 	return -1;
 }
-char followSeprime[8][10] = {"==", "!=", "<=", ">=", "<", ">", "id", "}"};
+char followSeprime[7][10] = {"==", "!=", "<=", ">=", "<", ">", ";"};
 int findfollowSeprime(char *str) {
-	for(int i=0; i<8; i++)
+	for(int i=0; i<7; i++)
 		if(strcmp(followSeprime[i], str)==0) return i;
 	return -1;
 }
@@ -172,12 +172,12 @@ int findfirstEprime(char *str) {
 		if(strcmp(firstEprime[i], str)==0) return i;
 	return -1;
 }
-char followEprime[2][10] = {"id", "}"};
-int findfollowEprime(char *str) {
-	for(int i=0; i<2; i++)
-		if(strcmp(followEprime[i], str)==0) return i;
-	return -1;
-}
+char* followEprime = ";";
+// int findfollowEprime(char *str) {
+// 	for(int i=0; i<2; i++)
+// 		if(strcmp(followEprime[i], str)==0) return i;
+// 	return -1;
+// }
 
 char firstMulop[3][10] = {"*", "/", "%"};
 int findfirstMulop(char *str) {
@@ -200,6 +200,7 @@ void declarations() {
 	if(strcmp(firstIDList, t->tokenName)==0)
 		identifier_list();
 	else return;
+	// printf("check - %s\n", t->tokenName); //////////////////////////////
 	if(strcmp(";", t->tokenName)==0) {
 		t = getToken(fptr);
 		printToken();
@@ -287,10 +288,16 @@ identifier_list_prime_prime() {
 }
 
 void statementList() {
+	// printf("statementList check - %s\n", t->tokenName); //////////////////////////////
 	statement();
+	// printf("check - %s\n", t->tokenName);
 	t = getToken(fptr);
 	printToken();
-	if(strcmp(firstStatementList, t->tokenName)==0) statementList();
+	// printf("check - %s\n", t->tokenName);
+	if(strcmp(firstStatementList, t->tokenName)==0) {
+		// printf("check firstStatementList - %s\n", t->tokenName);
+		statementList();
+	}
 	else if(strcmp(followStatementList, t->tokenName)==0) return;
 	else {
 		printf("Error at row: %d, col: %d, expected \"%s\" or \"%s\".\n", t->row, t->col, "id", "}");
@@ -299,13 +306,18 @@ void statementList() {
 }
 
 void statement() {
+	// printf("statement check - %s\n", t->tokenName); //////////////////////////////
 	assignStat();
 }
 
 void assignStat() {
+	// printf("assignStat check - %s\n", t->tokenName); //////////////////////////////
 	t = getToken(fptr);
 	printToken();
-	if(strcmp(t->tokenName, "=")==0) expn();
+	if(strcmp(t->tokenName, "=")==0) {
+		// printf("inside expn check - %s\n", t->tokenName); //////////////////////////////
+		expn();
+	}
 	else {
 		printf("Error at row: %d, col: %d, expected \"%s\".\n", t->row, t->col, "=");
 		exit(0);
@@ -315,11 +327,14 @@ void assignStat() {
 void expn() {
 	t = getToken(fptr);
 	printToken();
+	// printf("check expn - %s\n", t->tokenName);
+	// printf("ehbvhvgfwejbfbgff");
 	if(findfirstExpn(t->tokenName)!=-1) {
+		// printf("inside first of expn check - %s\n", t->tokenName); //////////////////////////////
 		simpleExpn();
 		if(findfirstEprime(t->tokenName)!=-1)
 			ePrime();
-		else if(findfollowEprime(t->tokenName)!=-1) return;
+		else if(strcmp(followEprime, t->tokenName)==0) return;
 		else {
 			printf("Error at row: %d, col: %d, expected \"%s\", \"%s\", \"%s\", \"%s\", \"%s\", \"%s\", \"%s\", or \"%s\".\n", t->row, t->col, "id", "}", "==", "!=", "<=", ">=", "<", ">");
 			exit(0);
@@ -333,6 +348,9 @@ void expn() {
 
 void simpleExpn() {
 		term();
+		// printf("check beta - %s\n", t->tokenName);
+		// printf("after term check - %s\n", t->tokenName); //////////////////////////////
+		// extra ; encountered
 		if(findfirstSeprime(t->tokenName)!=-1) sePrime();
 		else if(findfollowSeprime(t->tokenName)!=-1) return;
 		else {
@@ -342,8 +360,10 @@ void simpleExpn() {
 }
 
 void ePrime() {
+	// printf("check - %s\n", t->tokenName); //////////////////////////////
 		t = getToken(fptr);
 		printToken();
+		// printf("check - %s\n", t->tokenName); //////////////////////////////
 		if(findfirstSimpleExpn(t->tokenName)!=-1)
 			simpleExpn();
 		else {
@@ -353,10 +373,19 @@ void ePrime() {
 }
 
 void term() {
+	// printf("check - %s\n", t->tokenName); //////////////////////////////
+	// printf("check factor - %s\n", t->tokenName);
 	factor();
 	t = getToken(fptr);
 	printToken();
-	if(findfirstTprime(t->tokenName)!=-1) tPrime();
+	// printf("check after factor - %s\n", t->tokenName);
+	// printf("after factor check - %s - %d\n", t->tokenName, findfirstTprime(t->tokenName)); //////////////////////////////
+	// printf("check - %s %d\n", t->tokenName, findfirstTprime(t->tokenName)); //////////////////////////////
+	if(findfirstTprime(t->tokenName)!=-1) {
+		// printf("tprime check 123 - %s\n", t->tokenName); //////////////////////////////
+		tPrime();
+		// printf("after tprime check - %s\n", t->tokenName); //////////////////////////////
+	}
 	else if(findfollowTprime(t->tokenName)!=-1) return;
 	else {
 		printf("Error at row: %d, col: %d, expected \"%s\", \"%s\", \"%s\", \"%s\", \"%s\", \"%s\", \"%s\", \"%s\", \"%s\", \"%s\", \"%s\", \"%s\" or \"%s\".\n", t->row, t->col, "*", "/", "%", "id", "}", "+", "-", "==", "!=", "<=", ">=", "<", ">");
@@ -365,16 +394,26 @@ void term() {
 }
 
 void sePrime() {
-	t = getToken(fptr);
-	printToken();
+	// t = getToken(fptr);
+	// printToken();
+	printf("check seprime - %s\n", t->tokenName);
 	if(findfirstAddop(t->tokenName)==-1) {
+		printf("error found %s", t->tokenName);
 		printf("Error at row: %d, col: %d, expected \"%s\" or \"%s\".\n", t->row, t->col, "+", "-");
 		exit(0);
 	}
+	else if(findfollowSeprime(t->tokenName)!=-1) return;
 	t = getToken(fptr);
 	printToken();
+	printf("primealphabeta - %s\n", t->tokenName);
 	term();
-	sePrime();
+	// sePrime();
+	if(findfirstSeprime(t->tokenName)!=-1) sePrime();
+	else if(findfollowSeprime(t->tokenName)!=-1) return;
+	else {
+		printf("Error at row: %d, col: %d, expected \"%s\", \"%s\", \"%s\", \"%s\", \"%s\", \"%s\", \"%s\", \"%s\", \"%s\", or \"%s\".\n", t->row, t->col, "id", "}", "+", "-", "==", "!=", "<=", ">=", "<", ">");
+		exit(0);
+	}
 }
 
 void factor() {
@@ -382,17 +421,22 @@ void factor() {
 		printf("Error at row: %d, col: %d, expected \"%s\" or \"%s\".\n", t->row, t->col, "id", "NUM");
 		exit(0);
 	}
+	// printf("alpha check - %s\n", t->tokenName); //////////////////////////////
 }
 
 void tPrime() {
 	t = getToken(fptr);
 	printToken();
+	// printf("tprime fn check - %s\n", t->tokenName); //////////////////////////////
 	factor();
 	t = getToken(fptr);
 	printToken();
+	// if(strcmp(";", t->tokenName)==0) return; /////////////////////////
+	// printf("hjibfisdhbf dfkih fswdf check - %s\n", t->tokenName); //////////////////////////////
 	if(findfirstTprime(t->tokenName)!=-1) tPrime();
 	else if (findfollowTprime(t->tokenName)!=-1) return;
 	else {
+		// printf("checkalphaomega");
 		printf("Error at row: %d, col: %d, expected \"%s\", \"%s\", \"%s\", \"%s\", \"%s\", \"%s\", \"%s\", \"%s\", \"%s\", \"%s\", \"%s\", \"%s\" or \"%s\".\n", t->row, t->col, "*", "/", "%", "id", "}", "+", "-", "==", "!=", "<=", ">=", "<", ">");
 		exit(0);
 	}
@@ -418,7 +462,9 @@ void program() {
 					t = getToken(fptr);
 					printToken();
 					if(findfirstDeclaration(t->tokenName)!=-1) {
+						// printf("before declarations check - %s\n", t->tokenName); //////////////////////////////
 						declarations();
+						// printf("after declarations inside first declarations check - %s\n", t->tokenName); //////////////////////////////
 						if(strcmp(firstStatementList, t->tokenName)==0) statementList();
 						else if(strcmp(followStatementList, t->tokenName)==0) return;
 						else {
@@ -427,6 +473,7 @@ void program() {
 						}
 					}
 					else if(findfollowDeclaration(t->tokenName)!=-1) {
+						// printf("after declarations follow declarations check - %s\n", t->tokenName); //////////////////////////////
 						if(strcmp(firstStatementList, t->tokenName)==0) statementList();
 						else if(strcmp(followStatementList, t->tokenName)==0) return;
 						else {
